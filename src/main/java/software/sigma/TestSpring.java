1c74e4ae075da737b283824bbc8c764ef39d8f36
Bohdan Lesyk <bohdan.lesyk@sigma.software>
2022-04-25 18:58:13 +0300

    Lesson 8. Bean lifecycle (init, destroy and factory methods)

--- src/main/java/software/sigma/TestSpring.java
+++ src/main/java/software/sigma/TestSpring.java
@@ -5,21 +5,9 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class TestSpring {
     public static void main(String[] args) {
         try (ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml")) {
-            MusicPlayer firstMusicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
-            MusicPlayer secondMusicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
+            ClassicalMusic classicalMusic = context.getBean("classicalMusicBean", ClassicalMusic.class);
 
-            System.out.println(firstMusicPlayer == secondMusicPlayer);
-
-            System.out.println(firstMusicPlayer);
-            System.out.println(secondMusicPlayer);
-
-            System.out.println(firstMusicPlayer.getVolume());
-            System.out.println(secondMusicPlayer.getVolume());
-
-            firstMusicPlayer.setVolume(10);
-
-            System.out.println(firstMusicPlayer.getVolume());
-            System.out.println(secondMusicPlayer.getVolume());
+            System.out.println(classicalMusic.getSong());
         }
     }
 }
