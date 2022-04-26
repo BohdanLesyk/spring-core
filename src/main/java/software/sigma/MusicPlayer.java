58a514862262da34d4a05da9a2483d83ca8491a2
Bohdan Lesyk <bohdan.lesyk@sigma.software>
2022-04-26 21:45:21 +0300

    Lesson 9. Annotations. DI, part 1. @Autowired annotation (with field, setter and constructor)

--- src/main/java/software/sigma/MusicPlayer.java
+++ src/main/java/software/sigma/MusicPlayer.java
@@ -1,10 +1,27 @@
 package software.sigma;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 import java.util.LinkedList;
 import java.util.List;
 
+@Component
 public class MusicPlayer {
     private List<Music> musicList = new LinkedList<>();
+
+//    @Autowired
+    private Music music;
+
+    private ClassicalMusic classicalMusic;
+    private RockMusic rockMusic;
+    private JazzMusic jazzMusic;
+
+    @Autowired
+    public MusicPlayer(ClassicalMusic classicalMusic, RockMusic rockMusic, JazzMusic jazzMusic) {
+        this.classicalMusic = classicalMusic;
+        this.rockMusic = rockMusic;
+        this.jazzMusic = jazzMusic;
+    }
 
     private String name;
     private int volume;
@@ -13,10 +30,34 @@ public class MusicPlayer {
 
     }
 
-    public void playMusic() {
+//    @Autowired
+//    public MusicPlayer(Music music) {
+//        this.music = music;
+//    }
+//
+//    @Autowired
+//    public void setMusic(Music music) {
+//        this.music = music;
+//    }
+
+    public void playMusic(List<Music> musicList) {
         for (Music music : musicList) {
             System.out.println("Playing: " + music.getSong());
         }
+    }
+
+//    public void playMusic() {
+//        System.out.println("Playing: " + classicalMusic.getSong());
+//        System.out.println("Playing: " + rockMusic.getSong());
+//        System.out.println("Playing: " + jazzMusic.getSong());
+//    }
+
+//    public void playMusic() {
+//        System.out.println("Playing: " + music.getSong());
+//    }
+
+    public String playMusic() {
+        return "Playing: " + classicalMusic.getSong();
     }
 
     public void setMusicList(List<Music> musicList) {
